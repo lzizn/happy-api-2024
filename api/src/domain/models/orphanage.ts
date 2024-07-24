372d033de8f7a7129aa0b65fe34a2241c7481cff
Laian <braumlaian@gmail.com>
2024-07-24 12:06:17 -0300

    feat: add signature for OrphanageModel

---
+++ api/src/domain/models/orphanage.ts
@@ -0,0 +1,10 @@
+export interface OrphanageModel {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+  description: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+}
