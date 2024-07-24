e996c08c7be43f68df1c5e8e76516f318bdcd020
Laian <braumlaian@gmail.com>
2024-07-24 17:33:47 -0300

    feat: ensure app uses middlewares

--- api/src/main/config/app.ts
+++ api/src/main/config/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import setupMiddlewares from "./middlewares";
+
 const app = express();
 
 dotenv.config();
 
+setupMiddlewares(app);
 export default app;
