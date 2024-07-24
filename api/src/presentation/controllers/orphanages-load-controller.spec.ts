28dfd828a5685948b8bbb841e5f8f79c5daebcfb
Laian <braumlaian@gmail.com>
2024-07-24 12:15:02 -0300

    test: ensure OrphanagesLoadController calls OrphanagesLoad usecase

--- api/src/presentation/controllers/orphanages-load-controller.spec.ts
+++ api/src/presentation/controllers/orphanages-load-controller.spec.ts
@@ -1,14 +1,38 @@
+import { OrphanagesLoad } from "@/domain/usecases";
 import { OrphanagesLoadController } from "@/presentation/controllers";
 
-const makeSut = () => {
-  const sut = new OrphanagesLoadController();
+const makeOrphanagesLoad = () => {
+  class OrphanagesLoadStub implements OrphanagesLoad {
+    async load(): Promise<OrphanagesLoad.Result> {
+      return [];
+    }
+  }
 
-  return sut;
+  return new OrphanagesLoadStub();
+};
+
+const makeSut = () => {
+  const orphanagesLoad = makeOrphanagesLoad();
+  const sut = new OrphanagesLoadController(orphanagesLoad);
+
+  return {
+    sut,
+    orphanagesLoad,
+  } as const;
 };
 
 describe("Orphanages Load Controller", () => {
+  it("Should call orphanagesLoad", async () => {
+    const { sut, orphanagesLoad } = makeSut();
+
+    const orphanagesLoadSpy = jest.spyOn(orphanagesLoad, "load");
+
+    await sut.handle({});
+    expect(orphanagesLoadSpy).toHaveBeenCalled();
+  });
+
   it("Should return 200", async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const result = await sut.handle({});
     expect(result.statusCode).toBe(200);
   });
