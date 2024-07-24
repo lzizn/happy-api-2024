12efd617618d5d9e31fd1e320362efdfb1c76c47
Laian <braumlaian@gmail.com>
2024-07-24 12:21:51 -0300

    feat: add serverError http-helper (500 + internal server error message)

--- api/src/presentation/helpers/http-helper.ts
+++ api/src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from "@/presentation/errors";
 import type { HttpResponse } from "@/presentation/protocols";
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data,
+});
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(),
 });
