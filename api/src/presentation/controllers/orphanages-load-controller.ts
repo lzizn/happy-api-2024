7df36ca7218a971d1203327ec8b36e9ef1ef3817
Laian <braumlaian@gmail.com>
2024-07-24 12:10:39 -0300

    feat: ensure OrphanagesLoadController uses OrphanagesLoad usecase signature

--- api/src/presentation/controllers/orphanages-load-controller.ts
+++ api/src/presentation/controllers/orphanages-load-controller.ts
@@ -1,10 +1,16 @@
+import type { OrphanagesLoad } from "@/domain/usecases";
+
 import { ok } from "@/presentation/helpers";
 import type { Controller, HttpResponse } from "@/presentation/protocols";
 
 export class OrphanagesLoadController implements Controller {
+  constructor(private readonly orphanagesLoad: OrphanagesLoad) {}
+
   async handle(
     request: OrphanagesLoadController.Request
   ): Promise<HttpResponse> {
+    await this.orphanagesLoad.load();
+
     return ok({});
   }
 }
