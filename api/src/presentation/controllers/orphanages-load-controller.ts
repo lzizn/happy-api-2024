2ddaaab73b51afdbf7f01f5e9f2b381a6603fa1e
Laian <braumlaian@gmail.com>
2024-07-24 12:22:26 -0300

    feat: ensure OrphanagesLoadController throws when OrphanageLoad throws

--- api/src/presentation/controllers/orphanages-load-controller.ts
+++ api/src/presentation/controllers/orphanages-load-controller.ts
@@ -1,6 +1,6 @@
 import type { OrphanagesLoad } from "@/domain/usecases";
 
-import { ok } from "@/presentation/helpers";
+import { ok, serverError } from "@/presentation/helpers";
 import type { Controller, HttpResponse } from "@/presentation/protocols";
 
 export class OrphanagesLoadController implements Controller {
@@ -9,9 +9,13 @@ export class OrphanagesLoadController implements Controller {
   async handle(
     request: OrphanagesLoadController.Request
   ): Promise<HttpResponse> {
-    const orphanages = await this.orphanagesLoad.load();
+    try {
+      const orphanages = await this.orphanagesLoad.load();
 
-    return ok({ orphanages });
+      return ok({ orphanages });
+    } catch (e) {
+      return serverError();
+    }
   }
 }
 
