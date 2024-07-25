9a56f06595340222d58071e85be532075b4124b5
Laian <braumlaian@gmail.com>
2024-07-24 23:51:33 -0300

    feat: connect to mongodb when running server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,23 @@
 import "module-alias/register";
+import { configDotenv } from "dotenv";
 
-import { app } from "@/main/config/app";
+import { getEnv } from "@/main/config/env";
+import { MongoHelper } from "@/infra/db";
 
-app.listen(80, () => {
-  console.log("App is listening to port 80");
-});
+configDotenv();
+
+const env = getEnv();
+
+if (!env.database_url) {
+  throw new Error("DATABASE_URL variable is missing");
+}
+
+MongoHelper.connect(env.database_url)
+  .then(async () => {
+    const { app } = await import("./config/app");
+
+    app.listen(80, () => console.log(`Server running at http://localhost:80`));
+  })
+  .catch((x) => {
+    console.log("Failed to initialize server: ", x.message);
+  });
