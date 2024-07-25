3fa95fae3a0407cf029313f5afb96dad6e122a73
Laian <braumlaian@gmail.com>
2024-07-25 03:58:53 -0300

    feat: hookup OrphanageLoadResultController to /orphanages/:orphanageId/results

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,6 +1,7 @@
 import { Express, Router } from "express";
 
 import OrphanageRoutes from "@/main/routes/orphanage-routes";
+import OrphanageResultRoutes from "@/main/routes/orphanage-result-routes";
 
 export default (app: Express) => {
   const router = Router();
@@ -8,4 +9,5 @@ export default (app: Express) => {
   app.use("/api", router);
 
   OrphanageRoutes(router);
+  OrphanageResultRoutes(router);
 };
---
+++ src/main/routes/orphanage-result-routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { makeOrphanageLoadResultController } from "@/main/factories";
+import { adaptRoute } from "@/main/adapters";
+
+export default (router: Router) => {
+  router.get(
+    "/orphanages/:orphanageId/results",
+    adaptRoute(makeOrphanageLoadResultController())
+  );
+};
