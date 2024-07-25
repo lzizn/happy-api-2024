adb72e12faf4d6522118f9780901c587837eb265
Laian <braumlaian@gmail.com>
2024-07-25 00:52:09 -0300

    feat: add getCollection util fn to OrphanageMongoRepository in order to standardize collection shared by all methods

--- src/infra/db/mongodb/orphanage-mongo-repository.ts
+++ src/infra/db/mongodb/orphanage-mongo-repository.ts
@@ -1,13 +1,19 @@
+import { Collection } from "mongodb";
+
 import { MongoHelper } from "@/infra/db/mongodb";
 import type { OrphanageModel } from "@/domain/models";
 import type { OrphanagesLoadRepository } from "@/data/protocols/db";
 
 export class OrphanageMongoRepository implements OrphanagesLoadRepository {
+  getCollection(): Collection<OrphanageModel> {
+    return MongoHelper.getCollection<OrphanageModel>("orphanage");
+  }
+
   async loadAll(): Promise<OrphanagesLoadRepository.Result> {
-    const orphanagesCollection = MongoHelper.getCollection("orphanage");
+    const orphanagesCollection = this.getCollection();
 
     const orphanages = await orphanagesCollection.find().toArray();
 
-    return MongoHelper.mapCollection<OrphanageModel>(orphanages);
+    return MongoHelper.mapCollection(orphanages);
   }
 }
