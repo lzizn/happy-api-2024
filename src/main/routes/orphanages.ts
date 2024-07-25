33cda3e4cf66e64478d351e8c73ff84a7cc03aa8
Laian <braumlaian@gmail.com>
2024-07-25 01:39:34 -0300

    feat: hook /orphanages with OrphanagesLoadController

--- src/main/routes/orphanages.ts
+++ src/main/routes/orphanages.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
+import { adaptRoute } from "@/main/adapters";
+import { makeOrphanagesLoadController } from "@/main/factories";
+
 export const OrphanagesRoutes = (router: Router) => {
-  router.get("/orphanages", (_, res) => {
-    res.status(200).json({ orphanages: [] });
-  });
+  router.get("/orphanages", adaptRoute(makeOrphanagesLoadController()));
 };
