cff821229616538012c900d8cca30849d073dd14
Laian <braumlaian@gmail.com>
2024-07-25 04:44:52 -0300

    feat: ensure server db always has orphanages

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,10 @@
 import "module-alias/register";
+import { ObjectId } from "mongodb";
 import { configDotenv } from "dotenv";
 
 import { getEnv } from "@/main/config/env";
 import { MongoHelper } from "@/infra/db";
+import { mockOrphanageModels } from "@/domain/mocks";
 
 configDotenv();
 
@@ -13,10 +15,21 @@ if (!env.database_url) {
 }
 
 MongoHelper.connect(env.database_url)
-  .then(async () => {
+  .then(async (mongoClient) => {
     const { app } = await import("./config/app");
 
     app.listen(80, () => console.log(`Server running at http://localhost:80`));
+
+    const orphanageCollection = mongoClient.db().collection("orphanage");
+
+    const orphanagesAmount = await orphanageCollection.countDocuments();
+
+    if (orphanagesAmount === 0) {
+      const orphanagesSeed = mockOrphanageModels(20);
+      orphanageCollection.insertMany(
+        orphanagesSeed.map((x) => ({ ...x, _id: new ObjectId() }))
+      );
+    }
   })
   .catch((x) => {
     console.log("Failed to initialize server: ", x.message);
