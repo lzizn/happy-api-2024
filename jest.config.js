962652ed119bc2767e71539cb25a521bf17b5f20
Laian <braumlaian@gmail.com>
2024-07-25 00:59:17 -0300

    chore: jest: stop collecting coverage from index.ts files

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,7 @@
 module.exports = {
   preset: "@shelf/jest-mongodb",
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: [
-    "<rootDir>/src/**/*.ts",
-    "!<rootDir>/src/**/*-protocols.ts",
-  ],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/**/index.ts"],
   coveragePathIgnorePatterns: [
     "<rootDir>/src/main/server.ts",
     "<rootDir>/src/presentation/protocols/index.ts",
