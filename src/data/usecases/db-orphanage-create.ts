e07347bbe5cd3b6a0ca1386cd07d5321abac9dd9
Laian <braumlaian@gmail.com>
2024-07-26 18:42:17 -0300

    fix: rename repository variable on DbOrphanageCreate

--- src/data/usecases/db-orphanage-create.ts
+++ src/data/usecases/db-orphanage-create.ts
@@ -4,12 +4,12 @@ import type { OrphanageCreateRepository } from "@/data/protocols";
 
 export class DbOrphanageCreate implements OrphanageCreate {
   constructor(
-    private readonly orphanagesSaveRepository: OrphanageCreateRepository
+    private readonly orphanagesCreateRepository: OrphanageCreateRepository
   ) {}
 
   create(
     orphanage: Exclude<OrphanageModel, "id">
   ): Promise<OrphanageCreate.Result> {
-    return this.orphanagesSaveRepository.create(orphanage);
+    return this.orphanagesCreateRepository.create(orphanage);
   }
 }
