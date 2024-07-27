0d9c83d6adf8e147116ad82250fac94293589d64
Laian <braumlaian@gmail.com>
2024-07-27 11:58:12 -0300

    feat: add ValidationError

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,4 +1,5 @@
 export * from "./server-error";
 export * from "./not-found-error";
+export * from "./validation-error";
 export * from "./missing-param-error";
 export * from "./invalid-param-error";
