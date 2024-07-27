e28a05e997ba5ee79c2f490697152db85a879c6b
Laian <braumlaian@gmail.com>
2024-07-27 18:42:23 -0300

    refactor: SchemaValidator signature now returning ValidationError

--- src/data/protocols/db/schema-validator.ts
+++ src/data/protocols/db/schema-validator.ts
@@ -1,10 +1,15 @@
+import { ValidationError } from "@/presentation/errors";
+
+type GenericError = {
+  error: string[];
+};
+
 export type SchemaValidator<T> = {
-  isValid(object: T | Partial<T>): SchemaValidator.Result<T>;
+  validate(object: T | Partial<T>): SchemaValidator.Result<T>;
 };
 export namespace SchemaValidator {
   export type Result<T> =
-    | void
-    | {
-        [key in keyof T]?: string[];
-      };
+    | ValidationError<GenericError>
+    | ValidationError<T>
+    | void;
 }
