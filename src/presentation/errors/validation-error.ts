823820f8888ea5320c1bf349be4134a28ff44f85
Laian <braumlaian@gmail.com>
2024-07-27 18:20:43 -0300

    refactor: ValidationError, now accepting errors as param, including keys and error messages

--- src/presentation/errors/validation-error.ts
+++ src/presentation/errors/validation-error.ts
@@ -1,6 +1,13 @@
-export class ValidationError extends Error {
-  constructor(message: string) {
-    super(message);
+type ErrorItem<T> = {
+  [key in keyof T]: string[];
+};
+
+export class ValidationError<T> extends Error {
+  errors: ErrorItem<T>[] = [];
+
+  constructor(errors: ErrorItem<T>[]) {
+    super("Validation failed");
     this.name = "ValidationError";
+    this.errors = errors;
   }
 }
