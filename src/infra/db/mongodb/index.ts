497f2b6c560bb60fa2686bee668299f18abda88f
Laian <braumlaian@gmail.com>
2024-07-31 19:37:49 -0300

    refactor: delete query-builder

--- src/infra/db/mongodb/index.ts
+++ src/infra/db/mongodb/index.ts
@@ -1,5 +1,4 @@
 export * from "./mongo-helper";
-export * from "./query-builder";
 export * from "./log-mongo-repository";
 export * from "./orphanage-mongo-seed";
 export * from "./orphanage-mongo-repository";
