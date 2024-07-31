73473dc8f5de413e7b562768d0d5cdfc342c10d1
Laian <braumlaian@gmail.com>
2024-07-31 15:27:01 -0300

    feat: ensure app uses api-docs

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 
 import setupMiddlewares from "./middlewares";
 import setupRoutes from "./routes";
+import setupSwagger from "./swagger";
 
 const app = express();
 
 dotenv.config();
 
+setupSwagger(app);
 setupMiddlewares(app);
 setupRoutes(app);
 
